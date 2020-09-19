11fb3bc20ab26e710af253a787c93f3ba1b1d583
Gaurav Garre <gauravgarre@gmail.com>
2020-09-19 03:14:38 +0000

    Completed Exercise 1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 int main(int argc, char** argv) {
-	std::cout << "Use the wasd keys and enter to input a direction, or the E key to stop" << std::endl;
+	
 
     // Write your code for step 1 here
     std::vector<float> wheel_speeds(4);
