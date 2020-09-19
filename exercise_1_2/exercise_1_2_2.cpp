e1035087b3322d6035f70d30249454b903127f28
Gaurav Garre <gauravgarre@gmail.com>
2020-09-19 02:22:19 +0000

    Completed Exercise 1.2

--- exercise_1_2/exercise_1_2_2.cpp
+++ exercise_1_2/exercise_1_2_2.cpp
@@ -10,7 +10,22 @@ int main()
     std::cin >> str;
 
     // Palindrome Check
-    // WRITE YOUR PALINDROME CHECK HERE
+    int left = 0;
+    int right = str.length() - 1;
+    bool is_palindrome = true;
+    while (left < right) {
+    	if (str[left] != str[right]) {
+    		is_palindrome = false;
+    		break;
+    	}
+    	left++;
+    	right--;
+    }
+    std::string not_keyword = "NOT ";
+    if (is_palindrome) {
+    	not_keyword = "";
+    }
+    std::cout << str << " is " << not_keyword << "a palindrome" << std::endl;
 
     return 0;
 }
